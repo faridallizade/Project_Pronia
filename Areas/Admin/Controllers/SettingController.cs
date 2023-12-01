1b066bbc62c9f389dfd0e730489691534f425331
faridallizade <alizadebuzinez@gmail.com>
2023-12-01 12:30:21 +0400

    added SettingController and index view

---
+++ Areas/Admin/Controllers/SettingController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Project_Pronia.DAL;
+using Project_Pronia.Models;
+
+namespace Project_Pronia.Areas.Admin.Controllers
+{
+    [Area("Manage")]
+    public class SettingController : Controller
+    {
+
+        AppDbContext _context;
+        public SettingController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Setting> setting = _context.Settings.ToList();
+            return View(setting);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create (Setting setting)
+        {
+            if(!ModelState.IsValid)
+            {
+                _context.Settings.Add(setting);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+                return View(setting);
+        }
+
+
+        public IActionResult Update(int Id)
+        {
+            Setting setting = _context.Settings.Find(Id);
+            if(setting == null)
+            {
+                return View();
+            }
+            return View(setting);
+        }
+
+
+        [HttpPost]
+        public IActionResult Update(Setting newSetting)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View();
+            }
+            Setting oldSetting = _context.Settings.Find(newSetting.Id); 
+            if(oldSetting == null)
+            {
+                return View();
+            }
+            oldSetting.Key = newSetting.Key;
+            oldSetting.Value = newSetting.Value;
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete(int Id)
+        {
+            var setting = _context.Settings.FirstOrDefault(s=> s.Id == Id);
+            if (setting == null)
+            {
+                return View();
+            }
+            _context.Settings.Remove(setting);  
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
